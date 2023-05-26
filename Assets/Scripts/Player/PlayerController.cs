71b3f1676f07ed203c39a81f8f2665c6c287f176
Shuai Yuan <zczlsy6@ucl.ac.uk>
2023-05-26 01:14:35 +0100

    Update new features
    Add more rule tiles, add movement function and jump function.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInputControl inputControl;
+    private Rigidbody2D rb;
+    public Vector2 inputDirection;
+    
+    [Header("Basic Parameters")]
+    public float speed;
+    public float jumpForce;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputControl = new PlayerInputControl();
+
+        inputControl.Gameplay.Jump.started += Jump;
+    }
+
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+    
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+
+    private void Update()
+    {
+        inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // left and right movement
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
+
+        int faceDir = (int)transform.localScale.x;
+        if (inputDirection.x > 0)
+            faceDir = 1;
+        if (inputDirection.x < 0)
+            faceDir = -1;
+        // flip player
+        transform.localScale = new Vector3(faceDir, 1, 1);
+
+    }
+
+    private void Jump(InputAction.CallbackContext obj)
+    {
+        // Debug.Log("Jump!!!");
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+}
